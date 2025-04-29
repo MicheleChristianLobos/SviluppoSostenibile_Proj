34dae80cb07d6a4efbce2690c68b90bdbea1b9df
MicheleChristianLobos <lobosmichelechristian@gmail.com>
2025-04-29 10:00:35 +0200

    Prime modifiche

---
+++ Web_Service/moduli/frase_del_giorno.js
@@ -0,0 +1,34 @@
+//l'oggetto exports permette di trasmettere ed esportare 
+//ad un altro script dei dati attraverso la proprietà 
+//fraseDelGiorno che viene impostata con una frase scelta 
+//in modo casuale all'interno di un vettore di frasi
+//Per utilizzare il modulo che ho creato devo impostare nello script che lo utilizza
+//il suo riferimento ad una variabile: const miomodulo=require('./fraseDelGiorno.js')
+//se la frase invece di essere random fosse fissa il modulo sarebbe fatto in questo modo:
+//exports.fraseDelGiorno='Che cosa hanno in comune un televisore e una formica? Le antenne!';
+//tutte le funzioni non esportate nello script del modulo rimarranno private
+//possono pertanto essere utilizzate da altre funzioni
+//del modulo
+
+
+/*exports.fraseDelGiorno=function () {
+	const frase= [
+	'Che cosa hanno in comune un televisore e una formica? Le antenne!',
+	'Qual è la città preferita dai ragni? Mosca!',
+	'Qual è la pianta più puzzolente? Quella dei piedi!',
+	'Che cosa è una zebra? Un cavallo evaso dal carcere!',
+	'Sapete perché il pomodoro non riesce a dormire? Perché l’insalata… russa!'];
+	const indice=Math.floor(Math.random()*5);
+	return {
+		messaggio:frase[indice]
+	};
+}*/
+
+fetch('https://api.electricitymap.org/v3/carbon-intensity/latest?zone=IT', {
+    method: 'GET',
+    headers: {
+        'auth-token': 'RMJjL4r10v46DO6EElam'
+    }
+})
+.then(response => response.json())
+.then(data => console.log(data));
